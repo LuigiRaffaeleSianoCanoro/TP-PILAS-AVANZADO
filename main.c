402ba540def5cdcaa2a43f95b8851e8da13fe93d
lulito94 <38018740+lulito94@users.noreply.github.com>
2018-04-02 18:36:32 -0300

    Update main.c
    actualizr

--- main.c
+++ main.c
@@ -8,8 +8,8 @@ Un jugador gana la mano cuando la suma de sus cartas es mayor que las
 del contrario y al hacerlo coloca todas las cartas (las de el y las de
 su rival) en su pila de puntos. En caso de empate (y para simplificar)
 siempre gana el jugador1.
-Simular la ejecución del juego de tal manera que dada una pila MAZO
-(con un numero de elementos múltiplo de cuatro) distribuya
+Simular la ejecuciÃ³n del juego de tal manera que dada una pila MAZO
+(con un numero de elementos mÃºltiplo de cuatro) distribuya
 las cartas en las pilas PUNTOSJUG1 y PUNTOSJUG2 como si estos hubieran jugado. Utilizar
 las pilas auxiliares que crea conveniente. */
 
@@ -87,3 +87,4 @@ int main(){
 
     return 0;
 }
+fghjklÃ§lkjgf
